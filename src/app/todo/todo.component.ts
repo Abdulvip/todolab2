18721fe02fd6ecf610842820730ee90dfa58c771
Abdulvip <ahamdidueen@outlook.com>
2020-08-18 16:59:58 -0400

    lol

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../interfaces/todo';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css'],
+})
+export class TodoComponent implements OnInit {
+  todos: Todo[] = [
+    {
+      task: 'fold laundry',
+      completed: false,
+    },
+    {
+      task: 'exercise',
+      completed: false,
+    },
+    {
+      task: 'bake cookies',
+      completed: true,
+    },
+  ];
+
+  searchTerm: string;
+
+  showIndex: number;
+
+  constructor() {}
+
+  setShowIndex(index: number) {
+    this.showIndex = index;
+  }
+
+  addTask(form: NgForm) {
+    let newTodo: Todo = {
+      task: form.value.task,
+      completed: false,
+    };
+    this.todos.push(newTodo);
+    form.reset();
+  }
+
+  removeTask(index: number) {
+    this.todos.splice(index, 1);
+  }
+
+  completeTask(index: number) {
+    this.todos[index].completed = true;
+  }
+
+  setSearchTerm(form: NgForm) {
+    this.searchTerm = form.value.searchTerm.toLowerCase().trim();
+  }
+
+  myFilterMethod() {
+    if (!this.searchTerm) {
+      return this.todos;
+    } else {
+      return this.todos.filter((item) => {
+        return item.task.includes(this.searchTerm);
+      });
+    }
+  }
+
+  ngOnInit(): void {}
+}
